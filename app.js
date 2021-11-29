8e5afe97d962ca23af3f6eb5dfb2a14eb5bea249
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-29 14:52:17 -0700

    spelling error fixed in declarations and view role and view employee added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var inquirer = require('inquirer');
 const mysql = require("mysql")
 const cTable = require('console.table');
+const etrackerConnection = require('./db/connection');
 
 //Main Menu prompt- 1st Prompt
 
@@ -35,33 +36,53 @@ function promptUser() {
           viewAllEmployees();
         break;
 
-      case "View All Employee's By Roles?":
+      case "View All Employees By Role?":
           viewAllRoles();
         break;
-      case "View all Emplyees By Deparments":
+      case "View all Employees By Department":
           viewAllDepartments();
         break;
       
-      case "Add Employee?":
+      case "Add An Employee":
             addEmployee();
           break;
 
-      case "Update Employee":
+      case "Update An Employee":
             updateEmployee();
           break;
   
-        case "Add Role?":
+        case "Add A Role":
             addRole();
           break;
   
-        case "Add Department?":
+        case "Add A Department":
             addDepartment();
           break;
-
         }
 })
 }
 
+//View all employees route
+function viewAllEmployees() {
+    etrackerConnection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+  })
+}
+
+//View all Roles
+function viewAllRoles() {
+    etrackerConnection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+    })
+  }
+
+  // 
 
 // write code to link each of the prompts with the back end get/post/update routes
 promptUser().then(answers => console.log(answers));
